46fbdeb54fd1f8a1cf53ef0c4a4b835a7c471c3a
Matej Sychra <suculent@me.com>
2022-10-21 14:00:01 +0200

    upgraded bodyparser to express json

--- M1/server/app/index.js
+++ M1/server/app/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3333
-const bodyParser = require('body-parser');
 
-app.use(bodyParser);
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
