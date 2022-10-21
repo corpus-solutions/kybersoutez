03b66a81e982b0e1deb120a42c4d1899b951200e
Matej Sychra <suculent@me.com>
2022-10-21 13:28:38 +0200

    invocation fix

--- M1/server/build-and-run.sh
+++ M1/server/build-and-run.sh
@@ -6,7 +6,7 @@ docker build . -t suculent/nodetest
 
 # Run on port 3333 (interactively to enable Ctrl-C)
 
-docker run -ti -p 3333:3333 -t suculent/nodetest
+docker run -ti -p 3333:3333 suculent/nodetest
 
 # Test should return "Hello World!"
 
