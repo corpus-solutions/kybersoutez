edbd912db171e6baeb704839835a4f7a05cc27a0
Matej Sychra <suculent@me.com>
2024-02-08 17:18:51 +0100

    [android] successfully fetching list of pins through Burp Suite in dev mode

--- M4-SSL-Pinning/Android/app/src/main/java/cz/corpus/sslpinning/MainActivity.kt
+++ M4-SSL-Pinning/Android/app/src/main/java/cz/corpus/sslpinning/MainActivity.kt
@@ -1,22 +1,25 @@
 package cz.corpus.sslpinning
 
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
 import android.net.http.SslError
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.webkit.SslErrorHandler
-import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import java.io.ByteArrayInputStream
-import java.io.IOException
-import java.io.InputStream
-import java.nio.charset.StandardCharsets
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.security.KeyStore
+import java.security.cert.Certificate
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
 import java.util.*
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManagerFactory
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,9 +33,124 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         this.title = "CTF.2024"
 
+        Thread {
+            fetchPins()
+            verifyConnection()
+            runOnUiThread {}
+        }.start()
+
+        setupWebView()
+    }
+
+    /**
+     * Fetch pins using public resource (ssl-pinned)
+     */
+    private fun fetchPins() {
+
+        val url = URL("https://ssl.thinx.cloud/pin.json")
+
+        // Create SSLContext that uses no specific KeyManager or TrustManager
+        // (hopefully it will use system SSL pinning)
+        val context = SSLContext.getInstance("TLSv1.3")
+        //context.init(kmf.keyManagers, tmf.trustManagers, null)
+        context.init(null, null, null)
+
+        val urlConnection = url.openConnection() as HttpsURLConnection
+        urlConnection.sslSocketFactory = context.socketFactory
+
+        println("Fetching certificate pins...")
+
+        try {
+
+            val rd = BufferedReader(
+                InputStreamReader(urlConnection.inputStream)
+            )
+            var line: String?
+            while (rd.readLine().also { line = it } != null) {
+                Log.i("data", line!!)
+            }
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        } finally {
+            urlConnection.disconnect()
+        }
+        /*
+        var hurlConnection: HttpURLConnection? = null
+        try {
+            hurlConnection = url.openConnection() as HttpURLConnection
+            val code = hurlConnection.responseCode
+            if (code != 200) {
+                throw IOException("Invalid response from server: $code")
+            }
+            val rd = BufferedReader(
+                InputStreamReader(
+                    hurlConnection!!.inputStream
+                )
+            )
+            var line: String?
+            while (rd.readLine().also { line = it } != null) {
+                Log.i("data", line!!)
+            }
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        } finally {
+            hurlConnection?.disconnect()
+        }
+        */
+    }
+
+    private fun verifyConnection() {
+        try {
+            /*** CA Certificate  */
+            val cf = CertificateFactory.getInstance("X.509")
+            val caInput = resources.openRawResource(R.raw.trusted_roots)
+            val ca: Certificate = cf.generateCertificate(caInput)
+            System.out.println("ca=" + (ca as X509Certificate).getSubjectDN())
+
+            // Create a KeyStore containing our trusted CAs
+            val keyStoreType = KeyStore.getDefaultType()
+            val keyStore = KeyStore.getInstance(keyStoreType)
+            keyStore.load(null, null)
+            keyStore.setCertificateEntry("ca", ca)
+            println("keyStoreType:" + keyStoreType)
+
+            // Create a TrustManager that trusts the CAs in our KeyStore
+            val tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm()
+            val tmf = TrustManagerFactory.getInstance(tmfAlgorithm)
+            tmf.init(keyStore)
+            /*** Client Certificate  */
+            val keyStore12 = KeyStore.getInstance("PKCS12")
+            val certInput12 = resources.openRawResource(R.raw.alice)
+            keyStore12.load(certInput12, "sslpinning.corpus.cz".toCharArray()) // todo get bundleId
+
+            // Create a KeyManager that uses our client cert
+            val algorithm = KeyManagerFactory.getDefaultAlgorithm()
+            val kmf = KeyManagerFactory.getInstance(algorithm)
+            kmf.init(keyStore12, null)
+
+            /*** SSL Connection  */
+            // Create an SSLContext that uses our TrustManager and our KeyManager
+            val context = SSLContext.getInstance("TLSv1.2")
+            context.init(kmf.keyManagers, tmf.trustManagers, null)
+            val url = URL("https://192.168.10.10:4433/authenticate/")
+            val urlConnection = url.openConnection() as HttpsURLConnection
+            urlConnection.sslSocketFactory = context.socketFactory
+            println("Weeeeeeeeeee")
+            val ins = urlConnection.inputStream // this throw exception
+            println(ins.toString())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun setupWebView() {
         val myWebView: WebView = findViewById(R.id.webview)
-        val url = "http://192.168.10.10:3333/hello/" + UUID.randomUUID().toString()
         //val url = "http://192.168.10.10:3333/hello/" + UUID.randomUUID().toString()
+        //val url = "https://api.teacloud.net:3333/hello/" + UUID.randomUUID().toString()
+
+        // This should use HttpsURLConnection with client certificate
+        val url = "https://ctf24.teacloud.net/hello/" + UUID.randomUUID().toString()
+
         myWebView.loadUrl(url)
 
         myWebView.setWebViewClient(object : WebViewClient() {
@@ -67,9 +185,9 @@ class MainActivity : AppCompatActivity() {
 // https://community.letsencrypt.org/t/certificate-is-not-trusted-on-android/120061
 // Using cross-signed CA certificate in `trusted_roots` from https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem.txt
 
-// Dynamic Pinning: https://www.syxra.cz/pins.json
+// Dynamic Pinning: https://ssl.thinx.cloud/pin.json
 
 // TODO:
 // 1. Fetch certificate list from URL above
-// 2. Use OkHttp and CertificatePinner (https://www.netguru.com/blog/android-certificate-pinning)
+// 2. Potentially use OkHttp and CertificatePinner (https://www.netguru.com/blog/android-certificate-pinning)
 
